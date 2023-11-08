ec964e5f3e495db7a07685d936a9dcf9b0136d05
CristianNino <crnino@ucundinamarca.edu.co>
2023-11-07 20:32:21 -0500

    Update view.py

--- view/view.py
+++ view/view.py
@@ -24,17 +24,17 @@ class RegistrationApp(QMainWindow):
     # Configura el fondo azul utilizando una hoja de estilo
         style = """
             QMainWindow {
-                background-color: #3498db;
+                background-color: #5600A3;
             }
         
             QLabel {
-                color: white;
+                color: salmon;
             }
        
             QPushButton {
-                background-color: #27ae60;
-                color: white;
-                border: 2px solid #27ae60;
+                background-color: #4EB1F9;
+                color: black;
+                border: 2px solid #4EB1F9;
                 border-radius: 5px;
                 height: 40px;
             }
@@ -131,8 +131,9 @@ class RegistrationApp(QMainWindow):
 
         user = User(username, edad, sanguinidad, doc)  # Pasa el rol como tercer argumento
         self.save_user_data(user)
-        self.capture_and_save_facial_image(username,edad,sanguinidad,doc)
-
+        self.capture_and_save_facial_image(username, edad,  sanguinidad, doc)
+   
+    
 
     def save_user_data(self, user):
         username = user.username
@@ -145,7 +146,7 @@ class RegistrationApp(QMainWindow):
         with open(user_data_file, "a") as file:
             file.write(f"Nombre Completo: {username}, Edad: {edad}, Tipo de sangre: {sanguinidad}, Documento: {doc}\n")
 
-    def capture_and_save_facial_image(self, username, edad,sanguinidad, doc):
+    def capture_and_save_facial_image(self, username, edad, sanguinidad, doc):
         personName = username +" , " + edad + "," + sanguinidad + ","+ doc
         dataPath = 'fotosreconocimiento' 
         personPath = dataPath + '/' + personName
